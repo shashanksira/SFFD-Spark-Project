7d0211ed73c2c377e62b17f937503569886301ab
Shashank   Badavanahalli Rajashekar <shashanksira@gmail.com>
2019-02-09 21:32:16 -0500

    Update job1.scala

--- Code/ScalaCode/src/main/scala/job1.scala
+++ Code/ScalaCode/src/main/scala/job1.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.SparkSession
 object job1sub {
     val spark = SparkSession.builder().getOrCreate()
 
-    println("larkin")
+    println("In Job 1")
     def method1(): Unit = {
         val df = spark.read.csv("D:/Documents/sharpestminds/SparkProject/Data/FireDepartmentSample.csv")
         df.write.parquet("D:/Documents/sharpestminds/SparkProject/Data/Fire.parquet")
